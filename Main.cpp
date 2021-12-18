9d5b633a0010d7d355df660dad46ad55c525c817
Zemux1613 <42244022+Zemux1613@users.noreply.github.com>
2021-12-18 14:43:26 +0100

    feat: add small one times one

--- Main.cpp
+++ Main.cpp
@@ -1,4 +1,18 @@
+#include <stdio.h>
+
 int main() {
+
+    for (int i = 1; i < 11; i++) {
+        
+        printf("\nFor %d\n\n", i);
+        
+        for (int j = 1; j < 11; j++) {
+
+            printf("%d\n", i * j);
+        
+        }
+
+    }
 
     return 0;
 }
